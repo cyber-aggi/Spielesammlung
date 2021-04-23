f5df9b9e7492815e20731128a5bda3a76a1917a0
Niklas Pfadenhauer <niklaspfadenhauer@outlook.com>
2021-04-23 13:16:33 +0200

    Erstellt über ienen Button ein neues Form2

--- Spielesammlung/Spielesammlung/Form1.cs
+++ Spielesammlung/Spielesammlung/Form1.cs
@@ -12,9 +12,17 @@ namespace Spielesammlung
 {
     public partial class Form1 : Form
     {
+        private Form2 Childform;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void HighscoreButton_Click(object sender, EventArgs e)
+        {
+            Childform = new Form2();
+            Childform.Show();
         }
     }
 }
