7993ad3b2b3e400ef5fb0359ca499a7d4abe9bfd
Paul Rosenberg <mail@paulrosenberg.de>
2021-04-23 10:14:24 +0200

    Anwendungsname ändern

--- Spielesammlung/Spielesammlung/Form1.Designer.cs
+++ Spielesammlung/Spielesammlung/Form1.Designer.cs
@@ -48,7 +48,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Spielesammlung";
             this.ResumeLayout(false);
 
         }
