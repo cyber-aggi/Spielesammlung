9622e7ca486bbbb1bf12d6328860dea891ca34ff
Paul Rosenberg <mail@paulrosenberg.de>
2021-04-23 12:40:46 +0200

    Button gelöscht

--- Spielesammlung/Spielesammlung/Form1.cs
+++ Spielesammlung/Spielesammlung/Form1.cs
@@ -16,10 +16,5 @@ namespace Spielesammlung
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
