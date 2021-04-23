029a3514d6a6c56e29b6cceae7a1097459aa46d2
Paul Rosenberg <mail@paulrosenberg.de>
2021-04-23 13:03:39 +0200

    Grafik, Button, listBox

---
+++ Spielesammlung/Spielesammlung/Form2.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace Spielesammlung
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_load = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(452, 12);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(136, 23);
+            this.btn_load.TabIndex = 0;
+            this.btn_load.Text = "Highscore laden";
+            this.btn_load.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(25, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(563, 264);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_load);
+            this.Name = "Form2";
+            this.Text = "Highscore";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
