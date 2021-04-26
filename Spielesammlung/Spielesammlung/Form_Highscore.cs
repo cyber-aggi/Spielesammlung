195a32dd5c0e64a1fdecf362f1c3aa6b692435e3
prsnbrg <mail@paulrosenberg.de>
2021-04-26 08:35:59 +0200

    Exportfunktion für den Highscore

--- Spielesammlung/Spielesammlung/Form_Highscore.cs
+++ Spielesammlung/Spielesammlung/Form_Highscore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,14 @@ namespace Spielesammlung
         public Form_Highscore()
         {
             InitializeComponent();
+            //Lesen der Spieleliste aus der Properties / Settings Datei
             string spiele = Properties.Settings.Default.spiele;
             String[] value = null;
+            //Splitten der Informationen in ein Array
             value = spiele.Split(';');
             foreach(string v in value)
             {
+                //Array zur Combobox hinzufügen, wenn der Arrayeintrag nicht leer ist
                 if (v != "")
                 {
                     cB_spieleliste.Items.Add(v);
@@ -31,14 +35,15 @@ namespace Spielesammlung
         private void btn_load_Click(object sender, EventArgs e)
         {
             //Leert die Listbox bevor neue Einträge aus der Datenbank geholt werden
-            lB_Highscore.Items.Clear();
+            lW_Highscore.Items.Clear();
 
             //Holen der Einträge aus der Datenbank
+            //cB_spieleliste.Text
 
 
 
             //Eintragen der Datensätze in die Listbox
-            lB_Highscore.Items.Add("");
+            lW_Highscore.Items.Add("Test Eintrag");
 
             MessageBox.Show("Daten wurden erfolgreich geladen!", "Information");
         }
@@ -46,18 +51,56 @@ namespace Spielesammlung
         //Möglichkeit zum exportieren von  den Highscore Datensätzen
         private void btn_export_Click(object sender, EventArgs e)
         {
-            //Öffnet Dateiexplorer zum Speichern dieser Datei
-            SaveFileDialog sFD = new SaveFileDialog();
-
-            sFD.Filter = "CSV-Datei (*.csv)|*.csv|TXT-Datei (*.txt)|*.txt";
-            sFD.FilterIndex = 1;
-            sFD.RestoreDirectory = true;
-
-            //Wenn auf Speichern geklickt wurde, wird die Datei geschrieben
-            if (sFD.ShowDialog() == DialogResult.OK)
+            if (lW_Highscore.Items.Count == 0)
             {
-                //export_table(sFD.FileName + sFD.DefaultExt);
-                MessageBox.Show("Daten wurden erfolgreich exportiert!", "Information");
+                MessageBox.Show("Bitte lade zuerst den aktuellen Highscore aus der Datenbank!", "Fehlermeldung");
+            }
+            else
+            {
+                //Öffnet Dateiexplorer zum Speichern dieser Datei
+                SaveFileDialog sFD = new SaveFileDialog();
+
+                sFD.Filter = "CSV-Datei (*.csv)|*.csv|TXT-Datei (*.txt)|*.txt";
+                sFD.FilterIndex = 1;
+                sFD.RestoreDirectory = true;
+
+                //Wenn auf Speichern geklickt wurde, wird die Datei geschrieben
+                if (sFD.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        //Öffnen der Datei in der gespeichert werden soll
+                        StreamWriter writer = new StreamWriter(sFD.FileName + sFD.DefaultExt);
+
+                        //Füge jeden Eintrag in der Listbox zur Datei hinzu
+                        foreach (ListViewItem item in lW_Highscore.Items)
+                        {
+                            string output = "";
+                            int temp = 0;
+                            foreach (ListViewItem.ListViewSubItem row in item.SubItems)
+                            {
+                                if (temp == 0)
+                                {
+                                    output += row.Text.ToString();
+                                }
+                                else
+                                {
+                                    output += ";" + row.Text.ToString();
+                                }
+                                temp += 1;
+                            }
+                            writer.WriteLine(item);
+                        }
+                        //Schließen der Datei
+                        writer.Close();
+                    }
+                    catch (Exception error)
+                    {
+                        MessageBox.Show("Es gab einen Fehler: " + error.Message, "Fehlermeldung");
+                    }
+                    //Bestätigt den erfolgreichen Export der Daten aus dem ListView
+                    MessageBox.Show("Highscore wurde erfolgreich exportiert!", "Information");
+                }
             }
         }
     }
