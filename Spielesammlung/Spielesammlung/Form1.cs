1d2241ef891996084f1f09f88eaddf0d32111e75
Lena Krieger <lekrie@gmx.de>
2021-04-26 08:50:19 +0200

    Form1 verändert

--- Spielesammlung/Spielesammlung/Form1.cs
+++ Spielesammlung/Spielesammlung/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace Spielesammlung
     public partial class Form1 : Form
     {
         private Form_Highscore Childform;
+        private VierGewinntForm Childform2;
         
         public Form1()
         {
@@ -25,6 +27,31 @@ namespace Spielesammlung
             Childform = new Form_Highscore();
             Childform.Show();
             MessageBox.Show("Hier ein neues Fenster!");
+        }
+
+        private void StartenButton_Click(object sender, EventArgs e)
+        {
+            //Erzeugt ein neues Fenster um das Spiel anzuzeigen
+            if (Convert.ToString(Spiele_Liste.SelectedItem) == "Vier gewinnt")
+            {
+                Childform2 = new VierGewinntForm();
+                Childform2.Show();
+                MessageBox.Show("Viel Spaß beim Spielen!");
+            }
+            
+        }
+
+        private void LadenButton_Click(object sender, EventArgs e)
+        {
+            //Schreibt die Spiele aus einer Datei in die ListBox
+            StreamReader dateipointer = new StreamReader("");
+            for (int i = 0; i < 3; i++)
+            {
+                string zeile = dateipointer.ReadLine();
+
+                Spiele_Liste.Items.Add(zeile);
+            }
+            
         }
     }
 }
