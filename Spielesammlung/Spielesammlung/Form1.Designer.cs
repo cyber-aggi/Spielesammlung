f98e3f5fba3a473fe4db5ad8411af9b5ea36463a
Lena Krieger <lekrie@gmx.de>
2021-04-30 08:56:15 +0200

    Spielregeln ergänzt, Kniffelform ergänzt

--- Spielesammlung/Spielesammlung/Form1.Designer.cs
+++ Spielesammlung/Spielesammlung/Form1.Designer.cs
@@ -33,14 +33,20 @@
             this.StartenButton = new System.Windows.Forms.Button();
             this.LadenButton = new System.Windows.Forms.Button();
             this.RegelnButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Spieler1Name_Textbox = new System.Windows.Forms.TextBox();
+            this.Spieler2Name_TextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // HighscoreButton
             // 
-            this.HighscoreButton.Location = new System.Drawing.Point(823, 277);
+            this.HighscoreButton.Location = new System.Drawing.Point(685, 102);
             this.HighscoreButton.Margin = new System.Windows.Forms.Padding(6);
             this.HighscoreButton.Name = "HighscoreButton";
-            this.HighscoreButton.Size = new System.Drawing.Size(318, 67);
+            this.HighscoreButton.Size = new System.Drawing.Size(286, 57);
             this.HighscoreButton.TabIndex = 0;
             this.HighscoreButton.Text = "Highscore öffnen";
             this.HighscoreButton.UseVisualStyleBackColor = true;
@@ -57,9 +63,9 @@
             // 
             // StartenButton
             // 
-            this.StartenButton.Location = new System.Drawing.Point(821, 179);
+            this.StartenButton.Location = new System.Drawing.Point(797, 533);
             this.StartenButton.Name = "StartenButton";
-            this.StartenButton.Size = new System.Drawing.Size(320, 63);
+            this.StartenButton.Size = new System.Drawing.Size(339, 125);
             this.StartenButton.TabIndex = 2;
             this.StartenButton.Text = "Spiel starten";
             this.StartenButton.UseVisualStyleBackColor = true;
@@ -67,7 +73,7 @@
             // 
             // LadenButton
             // 
-            this.LadenButton.Location = new System.Drawing.Point(530, 91);
+            this.LadenButton.Location = new System.Drawing.Point(136, 102);
             this.LadenButton.Name = "LadenButton";
             this.LadenButton.Size = new System.Drawing.Size(234, 57);
             this.LadenButton.TabIndex = 3;
@@ -77,19 +83,65 @@
             // 
             // RegelnButton
             // 
-            this.RegelnButton.Location = new System.Drawing.Point(823, 382);
+            this.RegelnButton.Location = new System.Drawing.Point(390, 102);
             this.RegelnButton.Name = "RegelnButton";
-            this.RegelnButton.Size = new System.Drawing.Size(317, 75);
+            this.RegelnButton.Size = new System.Drawing.Size(272, 57);
             this.RegelnButton.TabIndex = 4;
             this.RegelnButton.Text = "Spielregeln";
             this.RegelnButton.UseVisualStyleBackColor = true;
             this.RegelnButton.Click += new System.EventHandler(this.RegelnButton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Spieler 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Spieler 2";
+            // 
+            // Spieler1Name_Textbox
+            // 
+            this.Spieler1Name_Textbox.Location = new System.Drawing.Point(150, 48);
+            this.Spieler1Name_Textbox.Name = "Spieler1Name_Textbox";
+            this.Spieler1Name_Textbox.Size = new System.Drawing.Size(163, 31);
+            this.Spieler1Name_Textbox.TabIndex = 7;
+            // 
+            // Spieler2Name_TextBox
+            // 
+            this.Spieler2Name_TextBox.Location = new System.Drawing.Point(150, 105);
+            this.Spieler2Name_TextBox.Name = "Spieler2Name_TextBox";
+            this.Spieler2Name_TextBox.Size = new System.Drawing.Size(163, 31);
+            this.Spieler2Name_TextBox.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Spieler1Name_Textbox);
+            this.groupBox1.Controls.Add(this.Spieler2Name_TextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(797, 341);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(339, 162);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Spielernamen";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1272, 865);
+            this.ClientSize = new System.Drawing.Size(1258, 765);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.RegelnButton);
             this.Controls.Add(this.LadenButton);
             this.Controls.Add(this.StartenButton);
@@ -98,6 +150,8 @@
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Spielesammlung";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -109,6 +163,11 @@
         private System.Windows.Forms.Button StartenButton;
         private System.Windows.Forms.Button LadenButton;
         private System.Windows.Forms.Button RegelnButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Spieler1Name_Textbox;
+        private System.Windows.Forms.TextBox Spieler2Name_TextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
