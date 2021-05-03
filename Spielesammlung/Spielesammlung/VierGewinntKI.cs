26da6abf40374f1a65836c6b1377a06245bdd195
Arthur <you@example.comagtarthur@web.de>
2021-05-03 07:39:48 +0200

    Fuege Klassen zur Kommunikation zwischen VierGewinntKI und VierGewinntForm hinzu

--- Spielesammlung/Spielesammlung/VierGewinntKI.cs
+++ Spielesammlung/Spielesammlung/VierGewinntKI.cs
@@ -8,6 +8,23 @@ namespace Spielesammlung
 {
     class VierGewinntKI
     {
+        Random rnd = new Random();
 
+        VierGewinntForm ui;
+
+        public VierGewinntKI(VierGewinntForm ui)
+        {
+            this.ui = ui;
+        }
+
+        public void MacheZug()
+        {
+            ui.DrawBlock(rnd.Next(1, 8));
+        }
+
+        public void GebeZug(int spalte)
+        { 
+            
+        }
     }
 }
